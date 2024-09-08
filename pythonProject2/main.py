ff13ce279a1171fdd10b37e0a2d8cf77e6fa0121
vodolej48 <sergej.trushin2010@yandex.ru>
2024-09-08 09:53:25 +0300

    9.09

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,2 +1,12 @@
-print(5/5)#
 
+
+#"1st progrfm"
+print(9 ** 0.5 * 5)
+#"2nd program"
+print(9.99 > 9.98, 1000 != 1000.1)
+#"3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+#"4th program"
+print(int(float('123.456') * 10)%10)
